43bfade1f0aac9bf0d26c13137f297eb7356d522
TortleTurtle <danielmeijs@gmail.com>
2020-06-06 18:07:56 +0200

    now ignoring the imgs in the data folder. Can now test retrained model and plot results

--- app.py
+++ app.py
@@ -10,7 +10,6 @@ from keras.applications import imagenet_utils
 from sklearn.metrics import confusion_matrix
 import itertools
 import matplotlib.pyplot as plt
-# %matplotlib inline
 
 # relative paths to the folders containing images
 train_path = 'data/train'
@@ -19,8 +18,10 @@ test_path = 'data/test'
 
 # prepocess images and create batches of the data
 train_batches = ImageDataGenerator().flow_from_directory(directory=train_path, target_size=(224, 224), classes=['engine','ship'], batch_size=10)
-valid_batches = ImageDataGenerator().flow_from_directory(directory=valid_path, target_size=(224, 224), classes=['engine','ship'], batch_size=10)
-test_batches = ImageDataGenerator().flow_from_directory(directory=test_path, target_size=(224, 224), classes=['engine','ship'], batch_size=7, shuffle=False)
+valid_batches = ImageDataGenerator().flow_from_directory(directory=valid_path, target_size=(224, 224), classes=['engine','ship'], batch_size=8)
+test_batches = ImageDataGenerator().flow_from_directory(directory=test_path, target_size=(224, 224), classes=['engine','ship'], batch_size=20, shuffle=False)
+
+print(train_batches.class_indices)
 
 # load mobileNet model
 mobile = keras.applications.mobilenet.MobileNet()
@@ -40,5 +41,49 @@ for layer in model.layers[:-5]:
 ### retraining the model
 # compile the model for training
 model.compile(optimizer=Adam(learning_rate=0.0001), loss='categorical_crossentropy', metrics=['accuracy'])
+# train the model
+model.fit_generator(generator=train_batches, steps_per_epoch=8, validation_data=valid_batches, validation_steps=1, epochs=30, verbose=2)
 
-model.fit_generator(generator=train_batches, steps_per_epoch=1, validation_data=valid_batches, validation_steps=1, epochs=30, verbose=2)
+### testing the model
+# Get the labels from the test batch
+test_labels = test_batches.classes
+predictions = model.predict_generator(test_batches, steps=1, verbose=0)
+
+# plotting predictions
+def plot_confusion_matrix(cm, classes,
+            normalize=False,
+            title='Confusion matrix',
+            cmap=plt.cm.get_cmap("Blues")):
+
+# This function prints and plots the confusion matrix.
+# Normalization can be applied by setting `normalize=True`.
+
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
+
+    print(cm)
+
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, cm[i, j],
+            horizontalalignment="center",
+            color="white" if cm[i, j] > thresh else "black")
+
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+    plt.show()
+
+cm = confusion_matrix(y_true=test_labels, y_pred=predictions.argmax(axis=1))
+cm_plot_labels = ['engine', 'ship']
+plot_confusion_matrix(cm, cm_plot_labels, title='Confusion Matrix')
